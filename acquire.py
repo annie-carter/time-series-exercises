555bba2e24ba1d9bfcaf9a5c03e172387691290a
annie-carter <annie.carter831@gmail.com>
2023-07-31 10:21:07 -0500

    complete

--- acquire.py
+++ acquire.py
@@ -33,5 +33,67 @@ def get_swpeople_data():
     
     df = pd.DataFrame(data)  # Create a DataFrame using the collected data
     return df
+
 #STARWARS PLANETS 
 '''This function gets starwars planets '''
+def get_planets_data():
+    # Initialize the URL for the first page
+    data = []
+    url = 'https://swapi.dev/api/planets/'  
+    
+    while True:
+        # Send a GET request to the SWAPI
+        response = requests.get(url)  
+        if response.status_code == 200:
+            json_data = response.json()
+            results = json_data['results']
+            data.extend(results)
+            url = json_data['next']  # Update the URL with the next page URL
+
+            if url is None:
+                break
+        else:
+            print(f"Error: {response.status_code}")
+            break
+    # Create a DataFrame using the collected data
+    df = pd.DataFrame(data)  
+    return df
+#STARWARS STARSHIPS  
+'''This function gets starwars starships '''
+def get_starships_data():
+    # Initialize the URL for the first page
+    data = []
+    url = 'https://swapi.dev/api/starships/'
+    
+    while True:
+        # Send a GET request to the SWAPI
+        response = requests.get(url)  
+        if response.status_code == 200:
+            json_data = response.json()
+            results = json_data['results']
+            data.extend(results)
+            url = json_data['next']  # Update the URL with the next page URL
+
+            if url is None:
+                break
+        else:
+            print(f"Error: {response.status_code}")
+            break
+    # Create a DataFrame using the collected data
+    df = pd.DataFrame(data)  
+    return df
+
+#STARWARS CVS  
+'''This function joins the above dataframes into one and makes cvs. '''
+
+def get_starwars_data(starwars_df):
+    '''This function creates a csv for concat wine csv'''
+    # Assuming you have a function 'get_starwars_data()' that retrieves the starwars data and returns a DataFrame
+    df_starwars = starwars_df
+
+    # Save the DataFrame to a CSV file
+    df_starwars.to_csv("starwars.csv", index=False)  # Specify 'index=False' to exclude the index column in the CSV
+
+    filename = 'starwars.csv'
+    if os.path.isfile(filename):
+        return pd.read_csv(filename)
